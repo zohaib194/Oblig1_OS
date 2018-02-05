bdd80df69d7dc8dda276b33a71acc517b6f4e1c4
Zohaib <zohaib_b13@hotmail.com>
2018-02-05 12:58:20 +0100

    initial commit

---
+++ Task1/oppgave_4b.c
@@ -0,0 +1,70 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+
+void process(int number, int time){
+	printf("Prosess %d kjrører\n", number);
+	sleep(time);
+	printf("Prosess %d kjørte i %d sekunder\n", number, time);
+}
+
+int main(void){
+	int p, p1, p2, p3;
+	
+	p=fork();
+	if (p < 0) {
+		printf("Error occured with process%d", p);
+	
+	} else if (p == 0) {
+		// child code
+		process(2,3);
+		
+		p2=fork();
+		if(p < 0) {
+			printf("Error occured with process%d", p);
+	
+		} else if(p2 == 0){
+			process(3, 2);
+			exit(0);		
+		} else {
+			waitpid(p2, NULL, 0);
+		}
+		exit(0);		// exit for process 2
+	} else {
+		// Parent code
+				
+		process(0, 1);
+		
+		p1 = fork();
+		if (p < 0) {
+			printf("Error occured with process%d", p);
+		} else if(p1 == 0){
+			process(1, 2);
+			exit(0);		// exit for process 1
+		} else {
+			process(4, 3);
+			waitpid(p1, NULL, 0);
+		
+			p3 = fork(); 
+			if (p < 0) { 
+				printf("Error occured with process%d", p);
+			} else if (p3 == 0) {
+				process(5, 3);
+				exit(0);		// exit for process 5
+			} else {
+				waitpid(p3, NULL, 0);
+			}
+		}
+	waitpid(p, NULL, 0);		
+	}
+	
+return 0;
+
+}
+
+
+
---
+++ Task2/oppgave_5a.c
@@ -0,0 +1,103 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define SHARED 0
+
+sem_t sem[6];
+
+struct threadargs {
+int id; /* thread number */
+int sec; /* how many seconds to sleep */
+int signal[6]; /* which threads to signal when done */
+};
+
+
+void *tfunc(void *arg) {
+	struct threadargs *targs=arg;
+	
+	// Waiting to enter critical region. 
+	sem_wait(&sem[targs->id]);
+	
+	// Print all the stuff to screen about a thread.
+	printf("Prosess %d kjører\n", targs->id);
+
+	printf("Prosess %d skal kjøre i %d sekunder\n", targs->id, targs->sec);
+
+	sleep(targs->sec);	// Sleep.
+
+	printf("Process %d er ferdig\n", targs->id); 	
+								// Done with critical region,
+	sem_post(&sem[targs->id]); 	// awake the threads waiting in sem_wait();
+	for(int i = 0; i < 6; i++){
+		if (targs->signal[i] == 1){	// Invoke the thread which needs to be invoked in signal
+			sem_post(&sem[i]);
+
+		}
+	}
+	pthread_exit(0);
+}
+
+int main(void){
+	int i,j;
+	struct threadargs *targs[6];
+	pthread_t threads[6];
+
+	/* allocate memory for threadargs and zero out semaphore signals */
+	for (i=0;i<6;i++) {
+		targs[i] = (struct threadargs*) malloc(sizeof(struct threadargs));
+		targs[i]->id = 0;
+		targs[i]->sec = 0;
+		for (j=0;j<6;j++) {
+			targs[i]->signal[j]=0;
+		}
+	}
+
+	/* Set data for all the threads */
+	for(i=0; i <6; i++){
+		targs[i]->id = i;
+		if(i == 0){
+			targs[i]->sec = 1;
+			targs[i]->signal[1] = 1;
+			targs[i]->signal[4] = 1;
+		}
+		if (i == 1 || i == 3) {
+			targs[i]->sec = 2;
+			if(i == 1) 
+			 targs[i]->signal[3] = 1; 
+		}
+		if (i == 2 || i == 4 || i == 5){
+			targs[i]->sec = 3;
+			if (i == 4)
+				targs[i]->signal[5] = 1;
+		}
+	}
+
+	/* Print all data of each thread */
+	for (int prt = 0 ; prt < 6; prt++){
+		printf("Id: %d, Seconds to sleep: %d, Signal: ,",targs[prt]->id, targs[prt]->sec);
+		for(int prt2 = 0; prt2 < 6; prt2++){
+			printf("%d,",targs[prt]->signal[prt2]);
+		}
+	}
+
+	/* Initializing and starting threads */
+	for(int x = 0; x < 6; x++){	
+		if(x == 0 || x == 2){
+			sem_init(&sem[x], SHARED, 1);
+		} else { 
+			// Sem, if its shared between process, is it going to wait for other threads
+			sem_init(&sem[x], SHARED, 0);
+		}
+		pthread_create(&threads[x], NULL, tfunc, (void*) targs[x]);
+	}
+
+	/* Wait for all threads */
+	for(int z = 5; z >= 0; z--){
+		pthread_join(threads[z], NULL);		
+	} 
+	printf("Done\n");
+
+	return 0;
+}
